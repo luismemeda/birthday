bbce365b8cdd0a4d2979210980924e74ae1e94ae
luismemeda <141699573+luismemeda@users.noreply.github.com>
2023-08-10 22:27:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,12 +30,12 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        turn_on: "亮个灯吧",
+        play: "来点音乐",
+        bannar_coming: "祝福走起",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        light_candle: "点蜡烛咯",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
